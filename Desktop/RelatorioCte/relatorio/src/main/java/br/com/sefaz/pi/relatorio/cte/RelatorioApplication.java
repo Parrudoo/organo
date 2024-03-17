2c19da6d8fc1250d736416ecf21870393ad84236
Diego Dias <diegodias1d@hotmail.com>
2024-03-17 17:30:26 -0300

    projeto inicial

---
+++ Desktop/RelatorioCte/relatorio/src/main/java/br/com/sefaz/pi/relatorio/cte/RelatorioApplication.java
@@ -0,0 +1,13 @@
+package br.com.sefaz.pi.relatorio.cte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatorioApplication.class, args);
+	}
+
+}
